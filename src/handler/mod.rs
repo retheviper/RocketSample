3afc3fa4e319d55dcba711c846d23149286cdbc8
retheviper <retheviper@gmail.com>
2022-03-30 21:14:39 +0900

    fix

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,7 +1,7 @@
 pub mod member_handler {
     use chrono::{DateTime, Utc};
-    use serde::Serialize;
     use rocket_contrib::json::Json;
+    use serde::Serialize;
 
     #[derive(Serialize)]
     pub struct Member {
@@ -13,22 +13,33 @@ pub mod member_handler {
         roles: Vec<String>,
     }
 
-    #[get("/api/v1/web/members/<id>")]
-    pub fn get_member(id: u32) -> Json<Member> {
+    #[get("/api/v1/web/members")]
+    pub fn list_member() -> Json<Vec<Member>> {
         Json(
-            Member {
-                id,
-                user_id: format!("member_id_{}", id).to_string(),
-                name: format!("member_{}", id).to_string(),
-                created_date: Utc::now(),
-                roles: vec!["member".to_string()],
-            }
+            (1..=10).map(|id| create_member_for_test(id)).collect()
         )
     }
 
+    #[get("/api/v1/web/members/<id>")]
+    pub fn get_member(id: u32) -> Json<Member> {
+        Json(
+            create_member_for_test(id)
+        )
+    }
+
+    fn create_member_for_test(id: u32) -> Member {
+        Member {
+            id,
+            user_id: format!("member_id_{}", id),
+            name: format!("member_{}", id),
+            created_date: Utc::now(),
+            roles: vec![String::from("member")],
+        }
+    }
+
     mod date_format {
-        use chrono::{DateTime, Utc, TimeZone};
-        use serde::{self, Deserialize, Serializer, Deserializer};
+        use chrono::{DateTime, TimeZone, Utc};
+        use serde::{self, Deserialize, Deserializer, Serializer};
 
         const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
 
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@
 #[macro_use]
 extern crate rocket;
 
+// use memory_db::{MemoryDB, HashKey};
+// use keccak_hasher::KeccakHasher;
+
 mod handler;
 
 fn main() {
-    rocket::ignite().mount("/", routes![handler::member_handler::get_member]).launch();
+    // let mut memory_db = MemoryDB::<KeccakHasher, HashKey<_ >, Vec<u8> >::default ();
+
+    rocket::ignite().mount("/", routes![
+        handler::member_handler::get_member,
+        handler::member_handler::list_member,
+    ]).launch();
 }
